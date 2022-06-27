b9455be80b8f28b21a01681cfae541a17152b945
tajriba <quvvatullayev@gmail.com>
2022-06-27 19:40:19 +0300

    String09.py

--- String09.py
+++ String09.py
@@ -8,5 +8,6 @@ def main(x1,x2,x3):
     Returns:
         str: return answer.
     """
-    return f'''"[{x1},{ x2}, {x3}]"'''
+    return "[" + str(x1) + "," + str(x2) + "," + str(x3) + "]"
+
 print(main(4,8,6))
